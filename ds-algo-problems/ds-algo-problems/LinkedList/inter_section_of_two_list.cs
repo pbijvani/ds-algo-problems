b96cb514f69d97b89742b70e4815049467a3aafa
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-06-20 23:35:54 -0400

    none

--- ds-algo-problems/ds-algo-problems/LinkedList/inter_section_of_two_list.cs
+++ ds-algo-problems/ds-algo-problems/LinkedList/inter_section_of_two_list.cs
@@ -88,6 +88,75 @@ namespace ds_algo_problems.LinkedList
             node2.next.next.next.next.next = point1;
 
             var res = GetIntersectingNode(node1, node2);
+
+            var res1 = GetIntersection(node1, node2);
+
         }
+
+        /*
+         * Same mathod. little different implementation.
+         * 6/20/2020
+         */
+
+        public Node GetIntersection(Node l1, Node l2)
+        {
+            if (l1 == null || l2 == null) return null;
+
+            var l1Res = GetNodeLenthAndTail(l1);
+            var l2Res = GetNodeLenthAndTail(l2);            
+
+            if (l1Res.Tail != l2Res.Tail) return null;
+
+            var shorter = l1Res.Length > l2Res.Length ? l2 : l1;
+            var longer = l1Res.Length > l2Res.Length ? l1 : l2;
+
+            var lenDiff = System.Math.Abs(l1Res.Length - l2Res.Length);
+
+            longer = GetKthNode(longer, lenDiff);
+
+            while(shorter != longer)
+            {
+                shorter = shorter.next;
+                longer = longer.next;
+            }
+
+            return longer;
+
+        }
+
+        public Node GetKthNode(Node node, int k)
+        {
+            for(int i = 0; i < k; i++)
+            {
+                node = node.next;
+            }
+
+            return node;
+        }
+
+        public NodeLenthAndTail GetNodeLenthAndTail(Node head)
+        {
+            if (head == null) return null;
+
+            var len = 1;
+
+            while(head.next != null)
+            {
+                len++;
+                head = head.next;
+            }
+
+            return new NodeLenthAndTail()
+            {
+                Length = len,
+                Tail = head
+            };
+        }
+    }
+
+    public class NodeLenthAndTail
+    {
+        public Node Tail { get; set; }
+        public int Length { get; set; }
     }
 }
