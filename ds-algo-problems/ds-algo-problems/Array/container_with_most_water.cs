d41db69deb17024d4ad8bf87e7c381baa2846dc5
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-07-25 17:17:10 -0400

    new

---
+++ ds-algo-problems/ds-algo-problems/Array/container_with_most_water.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.Array
+{
+    public class container_with_most_water
+    {
+        /*
+         * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+            Note: You may not slant the container and n is at least 2.
+            Example:
+
+            Input: [1,8,6,2,5,4,8,3,7]
+            Output: 49
+
+        https://leetcode.com/problems/container-with-most-water/
+         */
+
+        // Sol 1 : Compare wach element with other element. FInd the area and then find max area
+        // Its O(N2)
+
+        public void Test()
+        {
+            var res = ContainerWithMostWater(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+        }
+
+        public int ContainerWithMostWater(int[] height)
+        {
+            var len = height.Length;
+            if (len < 2) return 0;
+
+            var left = 0;
+            var right = len - 1;
+            var maxArea = int.MinValue;
+
+            while(left < right)
+            {
+                var isLeftShort = height[left] < height[right];
+                var area = (right - left) * (isLeftShort ? height[left] : height[right]);
+                maxArea = area > maxArea ? area : maxArea;
+                if(isLeftShort)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ ds-algo-problems/ds-algo-problems/String/Jewels_and_Stones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.String
+{
+    public class Jewels_and_Stones
+    {
+        /*
+         * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+
+         */
+
+        public int NumOfJewelsInStore(string j, string s)
+        {
+            HashSet<char> hashSet = new HashSet<char>();
+            foreach(var ch in j)
+            {
+                hashSet.Add(ch);
+            }
+
+            int count = 0;
+            foreach(var ch in s)
+            {
+                if(hashSet.Contains(ch))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
