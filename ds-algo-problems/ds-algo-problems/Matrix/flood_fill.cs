312437669eac882bcf1bd932afcf18fe10bfbfa7
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-07-26 15:47:20 -0400

    new

---
+++ ds-algo-problems/ds-algo-problems/Matrix/flood_fill.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.Matrix
+{
+    public class flood_fill
+    {
+        /*
+         * An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+            Example 1:
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+         */
+        public int[,] FloodFill(int[,] matrix, int sr, int sc, int newColor)
+        {
+            var srcColor = matrix[sr, sc];
+
+            if (srcColor == newColor) return matrix;
+
+            FillDFS(matrix, sr, sc, srcColor, newColor);
+
+            return matrix;
+        }
+
+        public void FillDFS(int[,] matrix, int row, int col, int srcColor, int newColor)
+        {
+            if (matrix[row, col] != srcColor) return;
+
+            matrix[row, col] = newColor;
+
+            int[] directionRow = new int[] { -1, 1, 0, 0 };
+            int[] directionCol = new int[] { 0, 0, 1, -1 };
+
+            for(int i = 0; i < directionCol.Length; i++)
+            {
+                var newRow = row + directionRow[i];
+                var newCol = col + directionCol[i];
+
+                if (newRow < 0 || newRow >= matrix.GetLength(0)) continue;
+                if (newCol < 0 || newCol >= matrix.GetLength(1)) continue;
+
+                FillDFS(matrix, newRow, newCol, srcColor, newColor);
+            }            
+        }
+    }
+}
---
+++ ds-algo-problems/ds-algo-problems/_Modrate/Array/Trapping_Rain_Water.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems._Modrate.Array
+{
+    public class Trapping_Rain_Water
+    {
+        /*
+         * Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+         * 
+         * Example:
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+https://leetcode.com/problems/trapping-rain-water/
+         */
+        public int Trap(int[] height)
+        {
+            var len = height.Length;
+            if (len < 3) return 0;
+
+            var trappedWater = 0;
+
+            var left = 0;
+            var right = len - 1;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            while(left < right)
+            {
+                var shortHeight = height[left] < height[right] ? height[left] : height[right];
+                var isLeftShort = height[left] < height[right] ? true : false;
+
+                if(shortHeight > 0)
+                    AddToDict(dict, left + 1, right - 1, shortHeight);
+
+                if(isLeftShort)
+                {
+                    var currLeft = height[left];
+                    left++;
+                    while(currLeft >= height[left] && left < right)
+                    {
+                        left++;
+                    }
+                }
+                else
+                {
+                    var currRight = height[right];
+                    right--;
+                    while(currRight >= height[right] && left < right)
+                    {
+                        right--;
+                    }
+                }
+                
+            }
+
+            foreach(var item in dict)
+            {
+                var curr = item.Value - height[item.Key];
+
+                trappedWater = trappedWater + (curr > 0 ? curr : 0);
+            }
+
+            return trappedWater;
+        }
+
+        private void AddToDict(Dictionary<int, int> dict, int from, int to, int height)
+        {
+            for(int i = from; i <= to; i++)
+            {
+                if(dict.ContainsKey(i))
+                {
+                    dict[i] = height;
+                }
+                else
+                {
+                    dict.Add(i, height);
+                }
+            }
+        }
+
+        // O(n) solution
+        public int Trap1(int[] height)
+        {
+            var len = height.Length;
+            if (len < 2) return 0;
+
+            var left = 0;
+            var right = len - 1;
+            var ans = 0;
+            var left_max = 0;
+            var right_max = 0;
+            while(left < right)
+            {
+                if(height[left] < height[right])
+                {
+                    if(height[left] >= left_max)
+                    {
+                        left_max = height[left];
+                    }
+                    else
+                    {
+                        ans = ans + left_max - height[left];
+                    }
+                    left++;
+                }
+                else
+                {
+                    if(height[right] > right_max)
+                    {
+                        right_max = height[right];
+                    }
+                    else
+                    {
+                        ans = ans + right_max - height[right];
+                    }
+                    right--;
+                }
+            }
+            return ans;
+        }
+    }
+}
