76df90d99cf863b84e4eda1a01c5028b6477bf61
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-08-02 16:59:39 -0400

    new

---
+++ ds-algo-problems/ds-algo-problems/LeetCodeTop/Graph_Matrix/connected_component_in_undirected_graph.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.LeetCodeTop.Graph_Matrix
+{
+    /*
+        In a undirected graph, find number of connected component
+
+        http://buttercola.blogspot.com/2016/01/leetcode-number-of-connected-components.html
+        https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+     
+     */
+    public class connected_component_in_undirected_graph
+    {
+        #region DSF
+        public int CountConnectedComponentDSF(int n, int[,] edges)
+        {
+            var adjList = new List<List<int>>();
+            var visited = new bool[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                adjList.Add(new List<int>());
+            }
+
+            var noOfEdges = edges.GetLength(0);
+
+            for(int i = 0; i < noOfEdges; i++)
+            {
+                var from = edges[i,0];
+                var to = edges[i,1];
+
+                adjList[from].Add(to);
+                adjList[to].Add(from);
+            }
+
+            int connectedComponent = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(!visited[i])
+                {
+                    DSFHelper(adjList, i, visited);
+                    connectedComponent++;
+                }
+            }
+
+            return connectedComponent;
+        }
+
+        public void DSFHelper(List<List<int>> adjList, int currV, bool[] visited)
+        {
+            visited[currV] = true;
+
+            foreach(var V in adjList[currV])
+            {
+                if(!visited[V])
+                {
+                    DSFHelper(adjList, V, visited);
+                }
+            }
+        }
+        #endregion
+
+        #region BFS
+
+        public int CountConnectedComponentBSF(int n, int[,] edges)
+        {
+            var adjList = new List<List<int>>();
+            var visited = new bool[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                adjList.Add(new List<int>());
+            }
+
+            var noOfEdges = edges.GetLength(0);
+
+            for (int i = 0; i < noOfEdges; i++)
+            {
+                var from = edges[i, 0];
+                var to = edges[i, 1];
+
+                adjList[from].Add(to);
+                adjList[to].Add(from);
+            }
+
+            var queue = new Queue<int>();
+            var connectedComponent = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                if(!visited[i])
+                {
+                    queue.Enqueue(i);
+
+                    while(queue.Any())
+                    {
+                        var queueItem = queue.Dequeue();
+                        visited[queueItem] = true;
+
+                        foreach(var v in adjList[queueItem])
+                        {
+                            if(!visited[v])
+                            {
+                                queue.Enqueue(v);
+                            }
+                        }
+                    }
+                    connectedComponent++;
+                }
+            }
+            return connectedComponent;
+        }
+        #endregion
+
+        public void test()
+        {
+            var arr = new int[,] { { 0, 1 }, { 1, 2 }, { 3, 4 } };
+
+            //var count = CountConnectedComponentDSF(5, arr);
+
+            var count = CountConnectedComponentBSF(5, arr);
+        }
+    }
+
+     
+}
