9197e7e2a5059a941b00d34dc651be976c08df8f
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-09-04 17:27:28 -0400

    new

--- ds-algo-problems/ds-algo-problems/Array/Find_k_closest_elements_of_given_number_in_array.cs
+++ ds-algo-problems/ds-algo-problems/Array/Find_k_closest_elements_of_given_number_in_array.cs
@@ -6,8 +6,112 @@ using System.Threading.Tasks;
 
 namespace ds_algo_problems.Array
 {
+    /*
+Given a sorted array arr, two integers k and x, find the k closest elements to x in the array. The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+ 
+
+https://leetcode.com/problems/find-k-closest-elements/      
+     */
     public class Find_k_closest_elements_of_given_number_in_array
     {
+        /*
+Time complexity : O(\log n +k)O(logn+k). O(\log n)O(logn) is for the time of binary search, while O(k)O(k) is for shrinking the index range to k elements.
+
+Space complexity : O(k)O(k). It is to generate the required sublist.         
+         */
+        public IList<int> FindClosestElements1(int[] arr, int k, int x)
+        {
+            var len = arr.Length;
+
+            if (len < k) return arr; 
+            if (x <= arr[0] || len == k) return SubArray(arr, 0, k - 1, k);
+            if (x > arr[len-1]) return SubArray(arr, len - k, len - 1, k);
+
+            var index = BS(arr, x);
+
+            var left = index;
+            var right = index;            
+
+            while(right - left + 1 < k)
+            {
+                var opt1 = left == 0 ? int.MaxValue : System.Math.Abs(arr[left - 1] - x);
+                var opt2 = right == arr.Length - 1 ? int.MaxValue : System.Math.Abs(arr[right + 1] - x);
+
+                if (opt1 <= opt2)
+                {
+                    left--;
+                }
+                else
+                {
+                    right++;
+                }
+            }
+
+            return SubArray(arr, left, right, k);
+        }
+
+        private int[] SubArray(int[] arr, int left, int right, int k)
+        {
+            var res = new int[k];
+            var indx = 0;
+            for (int i = left; i <= right; i++)
+            {
+                res[indx] = arr[i];
+                indx++;
+            }
+
+            return res;
+        }
+
+        public int BS(int[] arr, int x)
+        {
+            // Binary Search
+            // If x found return index of x
+            // If x not found, return index of closest ele of x
+
+            var left = 0;
+            var right = arr.Length - 1;
+            var mid = -1;
+
+            while(left <= right)
+            {
+                mid = (left + right) / 2;
+
+                if (arr[mid] == x) return mid;
+
+                if(x < arr[mid])
+                {
+                    right = mid - 1;
+                }
+
+                if(arr[mid] < x)
+                {
+                    left = mid + 1;
+                }
+            }
+
+            if (mid == 0)
+            {
+                return -1;
+            }
+            else 
+            {
+                return System.Math.Abs(arr[mid] - x) < System.Math.Abs(arr[mid - 1] - x) ? mid : mid - 1;
+            }
+
+        }
+
         public int[] FindClosestELement(int[] input, int k, int x)
         {
             int len = input.Length;
