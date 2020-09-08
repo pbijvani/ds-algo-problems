9ac61c8ab18e7410aa09aa9e201e22da272c5baa
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-09-08 12:01:37 -0400

    new

--- ds-algo-problems/ds-algo-problems/String/min_window_substring.cs
+++ ds-algo-problems/ds-algo-problems/String/min_window_substring.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace ds_algo_problems.String
 {
     /*
-     * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+     * Given a string S and a string T, 
+     * find the minimum window in S which will contain all the characters in T in complexity O(n).
 
 Example:
 
