770531914eda57c1cf5349c2c710183b28d927c5
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-09-18 23:19:36 -0400

    new

---
+++ ds-algo-problems/ds-algo-problems/amazon_online_assessment_problems/critical_routers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.amazon_online_assessment_problems
+{
+    /*
+     * https://leetcode.com/discuss/interview-question/436073/
+     * 
+          You are given an undirected connected graph. An articulation point (or cut vertex) is defined as a vertex which, when removed along with associated edges, makes the graph disconnected (or more precisely, increases the number of connected components in the graph). The task is to find all articulation points in the given graph.
+
+        Input:
+        The input to the function/method consists of three arguments:
+
+        numNodes, an integer representing the number of nodes in the graph.
+        numEdges, an integer representing the number of edges in the graph.
+        edges, the list of pair of integers - A, B representing an edge between the nodes A and B.
+        Output:
+        Return a list of integers representing the critical nodes.
+
+        Example:
+
+        Input: numNodes = 7, numEdges = 7, edges = [[0, 1], [0, 2], [1, 3], [2, 3], [2, 5], [5, 6], [3, 4]]
+     */
+
+    /*
+        Solution : https://www.youtube.com/watch?time_continue=1104&v=aZXi1unBdJA&feature=emb_logo
+        Bridges and Articulation points Algorithm | Graph Theory
+
+     */
+
+    class critical_routers
+    {
+    }
+}
