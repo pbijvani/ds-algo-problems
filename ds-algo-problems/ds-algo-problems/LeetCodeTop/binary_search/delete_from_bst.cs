9c584063d721d7b9ed5ed51be3e6e90353ce9555
Paresh Bijvani <paresh.bijvani@outlook.com>
2020-09-19 23:55:02 -0400

    new

---
+++ ds-algo-problems/ds-algo-problems/LeetCodeTop/binary_search/delete_from_bst.cs
@@ -0,0 +1,185 @@
+﻿using ds_algo_problems.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.LeetCodeTop.binary_search
+{
+    //https://leetcode.com/problems/delete-node-in-a-bst/
+    public class delete_from_bst
+    {
+
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null) return null;
+
+            if(key == root.data)
+            {
+                if(root.right == null)
+                {
+                    return root.left;
+                }
+                else if(root.left == null)
+                {
+                    return root.right;
+                }
+                else
+                {
+                    root.data = getMinVal(root.right);
+
+                    root.right = DeleteNode(root.right, root.data);
+                }                
+            }
+            else if (key < root.data) // left
+            {
+                root.left = DeleteNode(root.left, key);
+            }
+            else
+            {
+                root.right = DeleteNode(root.right, key);
+            }
+            return root;
+        }
+
+        private int getMinVal(TreeNode root)
+        {
+            int min = root.data;
+            while(root.left != null)
+            {
+                min = root.left.data;
+                root = root.left;
+            }
+            return min;
+        }
+
+        // Not passing all leetcode testcases
+        public TreeNode DeleteNodeIterative(TreeNode root, int key)
+        {
+            if (root == null) return root;
+            if (root.data == key && root.left == null && root.right == null) return null;
+            var curr = root;
+            var prev = root;
+
+            while (true)
+            {
+                if (key == curr.data)
+                {
+                    if (curr.left == null && curr.right == null)
+                    {
+                        if (prev.left == curr)
+                        {
+                            prev.left = null;
+                        }
+                        else
+                        {
+                            prev.right = null;
+                        }
+                        break;
+                    }
+                    else if (curr.left == null || curr.right == null)
+                    {
+                        if (curr.left == null)
+                        {
+                            if (prev.left == curr)
+                            {
+                                prev.left = curr.right;
+                            }
+                            else
+                            {
+                                prev.right = curr.right;
+                            }
+                        }
+                        if (curr.right == null)
+                        {
+                            if (prev.left == curr)
+                            {
+                                prev.left = curr.left;
+                            }
+                            else
+                            {
+                                prev.right = curr.left;
+                            }
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        if (curr.left.right == null)
+                        {
+                            if (prev.left == curr)
+                            {
+                                prev.left.data = curr.right.data;
+                                curr.right = null;
+                            }
+                            else
+                            {
+                                prev.right.data = curr.right.data;
+                                curr.right = null;
+                            }
+                        }
+                        else
+                        {
+                            var inorderPre = GetInOrderPredecessor(curr.left);
+
+                            curr.data = inorderPre;
+                        }
+
+                        break;
+                    }
+                }
+                else if (key < curr.data)
+                {
+                    prev = curr;
+                    curr = curr.left;
+                }
+                else
+                {
+                    prev = curr;
+                    curr = curr.right;
+                }
+
+                if (curr == null)
+                {
+                    break;
+                }
+            }
+
+            return root;
+        }
+
+        private int GetInOrderPredecessor(TreeNode root)
+        {
+            if (root.right.right == null)
+            {
+                var retVal = root.right.data;
+                root.right = null;
+                return retVal;
+            }
+            else
+            {
+                return GetInOrderPredecessor(root.right);
+            }
+        }
+
+        public void test()
+        {
+            var root = new TreeNode(5)
+            {
+                left = new TreeNode(3)
+                {
+                    left = new TreeNode(2),
+                    right = new TreeNode(4)
+                },
+                right = new TreeNode(6)
+                {
+                    left = null,
+                    right = new TreeNode(7)
+                }
+            };
+
+            var res = DeleteNode(root, 3);
+        }
+    }
+}
---
+++ ds-algo-problems/ds-algo-problems/LeetCodeTop/binary_search/insert_into_bst.cs
@@ -0,0 +1,143 @@
+﻿using ds_algo_problems.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds_algo_problems.LeetCodeTop.binary_search
+{
+    // https://leetcode.com/problems/insert-into-a-binary-search-tree/
+    /*
+        Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+        Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+        For example, 
+
+        Given the tree:
+                4
+               / \
+              2   7
+             / \
+            1   3
+        And the value to insert: 5
+        You can return this binary search tree:
+
+                 4
+               /   \
+              2     7
+             / \   /
+            1   3 5
+        This tree is also valid:
+
+                 5
+               /   \
+              2     7
+             / \   
+            1   3
+                 \
+                  4    
+     */
+    public class insert_into_bst
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+
+            var curr = root;
+
+            while(true)
+            {
+                if(val >= curr.data)
+                {
+                    // look into right
+
+                    if(curr.right == null)
+                    {
+                        curr.right = new TreeNode(val);
+                        break;
+                    }
+                    else
+                    {
+                        curr = curr.right;
+                    }
+
+                }
+                else
+                {
+                    if(curr.left == null)
+                    {
+                        curr.left = new TreeNode(val);
+                        break;
+                    }
+                    else
+                    {
+                        curr = curr.left;
+                    }
+                }
+            }
+
+            return root;
+        }
+
+        public void test()
+        {
+            var root = new TreeNode(50)
+            {
+                left = new TreeNode(30)
+                {
+                    left = new TreeNode(10),
+                    right = new TreeNode(20)
+                },
+                right = new TreeNode(80)
+                {
+                    left = new TreeNode(60),
+                    right = new TreeNode(90)
+                }
+
+            };
+
+            var res = InsertIntoBST(root, 48);
+        }
+
+        public TreeNode InsertIntoBSTRec(TreeNode root, int val)
+        {
+            if (root == null) return new TreeNode(val);
+
+            helper(root, val);
+
+            return root;
+        }
+
+        public void helper(TreeNode root, int val)
+        {
+            if(val < root.data)
+            {
+                if(root.left == null)
+                {
+                    root.left = new TreeNode(val);
+                    return;
+                }
+                else
+                {
+                    helper(root.left, val);
+                }
+            }
+            else
+            {
+                if (root.right == null)
+                {
+                    root.right = new TreeNode(val);
+                    return;
+                }
+                else
+                {
+                    helper(root.right, val);
+                }
+            }
+        }
+    }
+
+    
+}
